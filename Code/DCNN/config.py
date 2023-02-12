ba545ecdc0d488782e5c3f46786271f764213a7d
kol-di <60890747+kol-di@users.noreply.github.com>
2023-02-12 13:27:17 +0300

    global config paths

--- Code/DCNN/config.py
+++ Code/DCNN/config.py
@@ -7,3 +7,8 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 TRAIN_BATCH_SIZE = 8
 TEST_BATCH_SIZE = 16
 VAL_BATCH_SIZE = 64
+
+LR = 1e-3
+
+DATA_PATH = '/Users/koldi/se/DepressionPrediction/Psychiatric-Disorders-Data/Notebooks/psychiatric.disorders.ML'
+IMAGES_PATH = '/Users/koldi/se/DepressionPrediction/Depression_Spectrograms/Notebooks/spec_images_clean_6s_80freq'
